e55a1e8626ce47fc53987baf9ee7d02e9cd27991
Michael Wu <mwu230@bloomberg.net>
2017-10-24 13:04:40 -0400

    Abstract EK Client to cardGameClient

---
+++ app/models/cardGameClient.js
@@ -0,0 +1,200 @@
+var Backbone = require("backbone");
+var _ = require("lodash");
+var CardGameClient = Backbone.Model.extend({
+  initialize: function(options){
+    var self = this;
+    this.player = options.player;
+    this.socket = this.player.getSocket();
+    this.socket.off("roomInfo");
+    this.socket.on("roomInfo", function(roomInfo){
+      self.processRoomInfo(roomInfo);
+    });
+    this.socket.off("gameMessage");
+    this.socket.on("gameMessage", function(message){
+      self.processGameMessage(message);
+    });
+    //We do this instead of the server side event because of the delay it takes to create the view
+    this.getRoomInfo();
+  },
+
+  onSelfDraw: function(card){
+    this.get("gameState").deckCount--;
+    this.addCardToHand(card);
+  },
+
+  onOpponentDraw: function(id){
+    this.get("gameState").deckCount--;
+    this.getPlayerById(id).handSize++;
+  },
+
+  onUpdatePlayer: function(newPlayer){
+    this.get("gameState").turnPlayer = newPlayer;
+  },
+
+  getRoomInfo: function(){
+    this.socket.emit("requestRoomInfo");
+  },
+
+  isHost: function(){
+    return this.get("host") && this.isMe(this.get("host").id);
+  },
+
+  processRoomInfo: function(roomInfo){
+    var self = this;
+    Object.keys(roomInfo).forEach(function(val){
+      self.set(val, roomInfo[val]);
+    });
+    this.rotatePlayers();
+    this.trigger("update:room");
+  },
+
+  actions: {
+    playerDraw: function(message){
+      if(this.isMe(message.playerId) && message.card){
+        this.onSelfDraw(message.card);
+        this.trigger("self:draw", {
+          card: message.card
+        });
+      }else if(!this.isMe(message.playerId)){
+        this.onOpponentDraw(message.playerId);
+        this.trigger("opponent:draw", {
+          playerId: message.playerId
+        });
+      }
+    },
+    playerTurn: function(message){
+      this.onUpdatePlayer(message.player);
+      this.trigger("update:player", message.player);
+    },
+    moveCard: function(message){
+      this.onMoveCard(message);
+      this.trigger("card:move", message);
+    },
+    cardPlayed: function(message){
+      this.onCardPlayed(message);
+      this.trigger("card:played", message);
+    },
+    gameStart: function(message){
+      this.onGameStart();
+      this.trigger("game:start");
+    },
+    playerWin: function(message){
+      this.onPlayerWin(message);
+      this.trigger("player:win", message);
+    },
+    invalidCard: function(message){
+      this.trigger("card:invalid");
+    },
+  },
+
+  processGameMessage: function(message){
+    this.actions[message.message].call(this, message);
+  },
+
+  onGameStart: function(){
+    console.log("Game started");
+  },
+
+  playCard: function(options){
+    console.log("Playing card");
+  },
+
+  validatePlayable: function(card){
+    return true;
+  },
+
+  onCardPlayed: function(options){
+    console.log("Card played");
+  },
+
+  onPlayerWin: function(message){
+    this.set("status", 3);
+  },
+
+  removeCardFromHand: function(card){
+    var hand = this.get("gameState").hand;
+    for(var i=0; i<hand.length; i++){
+      if(hand[i].id == card.id && hand[i].image == card.image){
+        hand = hand.splice(i, 1);
+        this.getPlayerById(this.socket.id).handSize--;
+        return;
+      }
+    }
+    console.error("Tried to remove a card that doesn't exist");
+  },
+
+  getPlayerById: function(id){
+    return this.get("players").filter(function(player){
+      return player.id == id;
+    })[0];
+  },
+
+  getCardsInHand: function(card){
+    var gameState = this.get("gameState");
+    if(gameState && gameState.hand){
+      return gameState.hand.filter(function(handCard){
+        return handCard.id == card.id && handCard.image == card.image;
+      });
+    }
+    return [];
+  },
+
+  addCardToHand: function(card){
+    this.get("gameState").hand.push(card);
+    this.getPlayerById(this.socket.id).handSize++;
+  },
+
+  drawCard: function(){
+    if(this.inProgress() && this.isMyTurn()){
+      this.socket.emit("gameMessage", {
+        command: "drawCard",
+        roomId: this.get("id")
+      });
+    }
+  },
+
+  rotatePlayers: function(){
+    var players = this.get("players");
+    if(players && players.length){
+      while(!this.isMe(players[0].id)){
+        players.unshift(players.pop());
+      }
+    }
+  },
+
+  onMoveCard: function(options){
+    if(this.isMe(options.to)){
+      this.addCardToHand(options.card);
+      this.getPlayerById(options.from).handSize--;
+    }else if(this.isMe(options.from)){
+      this.removeCardFromHand(options.card);
+      this.getPlayerById(options.to).handSize++;
+    }else{
+      this.getPlayerById(options.from).handSize--;
+      this.getPlayerById(options.to).handSize++;
+    }
+  },
+
+
+  startRoom: function(){
+    this.socket.emit("gameMessage", {
+      command: "startGame",
+      roomId: this.get("id")
+    });
+  },
+
+  inProgress: function(){
+    return this.get("status") == 2;
+  },
+
+  isMyTurn: function(){
+    return this.get("gameState") && this.isMe(this.get("gameState").turnPlayer);
+  },
+
+  isMe: function(id){
+    return this.socket.id == id;
+  }
+
+});
+
+module.exports = CardGameClient;
--- app/models/explodingKittensClient.js
+++ app/models/explodingKittensClient.js
@@ -1,307 +1,137 @@
 var Backbone = require("backbone");
 var _ = require("lodash");
-var ExplodingKitten = Backbone.Model.extend({
-	initialize: function(options){
-		var self = this;
-		this.player = options.player;
-		this.socket = this.player.getSocket();
-		this.socket.off("roomInfo");
-		this.socket.on("roomInfo", function(roomInfo){
-			self.processRoomInfo(roomInfo);
-		});
-		this.socket.off("gameMessage");
-		this.socket.on("gameMessage", function(message){
-			self.processGameMessage(message);
-		});
-		//We do this instead of the server side event because of the delay it takes to create the view
-		this.getRoomInfo();
-	},
+var CardGameClient = require("./cardGameClient");
 
-	isExploding: false,
+var ExplodingKitten = CardGameClient.extend({
+  isExploding: false,
+  actions: _.extend(CardGameClient.prototype.actions, {
+    gaveFavor: function(message){
+      this.onMoveCard(message);
+      this.trigger("card:move", message);
+    },
+    seeTheFuture: function(message){
+      this.trigger("effect:stf", message);
+    },
+    doFavor: function(message){
+      this.trigger("do:favor", message);
+    },
+    drewExplodingKitten: function(message){
+      this.onEKDrawn(message);
+      this.trigger("ek:drawn", message);
+    },
+    defusedExplodingKitten: function(message){
+      this.onEKDefuse(message);
+      this.trigger("ek:defused", message);
+    },
+    exploded: function(message){
+      this.onPlayerExploded(message);
+      this.trigger("player:exploded", message);
+    },
+    setTimer: function(message){
+      this.trigger("timer:set", message);
+    },
+  }),
 
-	onSelfDraw: function(card){
-		this.get("gameState").deckCount--;
-		this.addCardToHand(card);
-	},
+  onGameStart: function(){
+    this.isExploding = false;
+  },
 
-	onOpponentDraw: function(id){
-		this.get("gameState").deckCount--;
-		this.getPlayerById(id).handSize++;
-	},
+  playCard: function(options){
+    if(options.card && this.validatePlayable(options.card)){
+      this.socket.emit("gameMessage", {
+        command: "playCard",
+        roomId: this.get("id"),
+        card: options.card,
+        target: options.target
+      });
+    }else{
+      this.trigger("card:invalid", {
+        card: options.card
+      });
+    }
+  },
 
-	onUpdatePlayer: function(newPlayer){
-		this.get("gameState").turnPlayer = newPlayer;
-	},
+  giveFavor: function(options){
+    this.socket.emit("gameMessage", {
+      command: "giveFavor",
+      roomId: this.get("id"),
+      card: options.card,
+      target: options.target
+    });
+  },
 
-	getRoomInfo: function(){
-		this.socket.emit("requestRoomInfo");
-	},
+  validatePlayable: function(card){
+    //validate that the current player can play this card
+    if(!this.inProgress()){
+      return false;
+    }
+    var gameState = this.get("gameState");
+    if(card.type != "nope" && !this.isMyTurn()){
+      return false;
+    }
+    if(this.isExploding && ["defuse", "nope"].indexOf(card.type) == -1){
+      return false;
+    }
+    var inHand = this.getCardsInHand(card);
+    if(card.type == "cat"){
+      return inHand.length > 1;
+    }else{
+      return inHand.length > 0;
+    }
+  },
 
-	isHost: function(){
-		return this.get("host") && this.isMe(this.get("host").id);
-	},
+  onEKDefuse: function(options){
+    this.isExploding = false;
 
-	processRoomInfo: function(roomInfo){
-		var self = this;
-		Object.keys(roomInfo).forEach(function(val){
-			self.set(val, roomInfo[val]);
-		});
-		this.rotatePlayers();
-		this.trigger("update:room");
-	},
+    //The kitten was placed back into deck
+    this.get("gameState").deckCount++;
+  },
 
-	processGameMessage: function(message){
-		switch(message.message){
-			case "playerDraw":
-				if(this.isMe(message.playerId) && message.card){
-					this.onSelfDraw(message.card);
-					this.trigger("self:draw", {
-						card: message.card
-					});
-				}else if(!this.isMe(message.playerId)){
-					this.onOpponentDraw(message.playerId);
-					this.trigger("opponent:draw", {
-						playerId: message.playerId
-					});
-				}
-				break;
-			case "playerTurn":
-				this.onUpdatePlayer(message.player);
-				this.trigger("update:player", message.player);
-				break;
-			case "moveCard":
-			case "gaveFavor":
-				this.onMoveCard(message);
-				this.trigger("card:move", message);
-				break;
-			case "cardPlayed":
-				this.onCardPlayed(message);
-				this.trigger("card:played", message);
-				break;
-			case "seeTheFuture":
-				this.trigger("effect:stf", message);
-				break;
-			case "doFavor":
-				this.trigger("do:favor", message);
-				break;
-			case "drewExplodingKitten":
-				this.onEKDrawn(message);
-				this.trigger("ek:drawn", message);
-				break;
-			case "defusedExplodingKitten":
-				this.onEKDefuse(message);
-				this.trigger("ek:defused", message);
-				break;
-			case "exploded":
-				this.onPlayerExploded(message);
-				this.trigger("player:exploded", message);
-				break;
-			case "gameStart":
-				this.onGameStart();
-				this.trigger("game:start");
-				break;
-			case "playerWin":
-				this.onPlayerWin(message);
-				this.trigger("player:win", message);
-				break;
-			case "setTimer":
-				this.trigger("timer:set", message);
-				break;
-			case "invalidCard":
-				this.trigger("card:invalid");
-				break;
-			default:
-				console.log("Not implemented:");
-				console.log(message);
-				break;
-		}
-	},
+  onPlayerExploded: function(message){
+    if(this.isMe(message.player)){
+      this.get("gameState").hand = [];
+    }
+    this.getPlayerById(message.player).handSize = 0;
+    if(!this.get("gameState").exploded){
+      this.get("gameState").exploded = [];
+    }
+    this.get("gameState").exploded.push(message.player);
+  },
 
-	onGameStart: function(){
-		this.isExploding = false;
-	},
+  isExploded: function(player){
+    return this.get("gameState").exploded && this.get("gameState").exploded.indexOf(player) > -1;
+  },
 
-	playCard: function(options){
-		if(options.card && this.validatePlayable(options.card)){
-			this.socket.emit("gameMessage", {
-				command: "playCard",
-				roomId: this.get("id"),
-				card: options.card,
-				target: options.target
-			});
-		}else{
-			this.trigger("card:invalid", {
-				card: options.card
-			});
-		}
-	},
+  onCardPlayed: function(options){
+    if(this.isMe(options.from)){
+      this.removeCardFromHand(options.card);
+      if(options.card.type == "cat"){
+        this.removeCardFromHand(options.card);
+      }
+    }else{
+      var p = this.getPlayerById(options.from);
+      p.handSize--;
+      if(options.card.type == "cat"){
+        p.handSize--;
+      }
+    }
+    this.get("gameState").pile.push(options.card);
+    if(options.card.type == "cat"){
+      this.get("gameState").pile.push(options.card);
+    }
+  },
 
-	giveFavor: function(options){
-		this.socket.emit("gameMessage", {
-			command: "giveFavor",
-			roomId: this.get("id"),
-			card: options.card,
-			target: options.target
-		});
-	},
+  onEKDrawn: function(message){
+    this.get("gameState").deckCount--;
+    if(this.isMe(message.player)){
+      this.isExploding = true;
+    }
+  },
 
-	validatePlayable: function(card){
-		//validate that the current player can play this card
-		if(!this.inProgress()){
-			return false;
-		}
-		var gameState = this.get("gameState");
-		if(card.type != "nope" && !this.isMyTurn()){
-			return false;
-		}
-		if(this.isExploding && ["defuse", "nope"].indexOf(card.type) == -1){
-			return false;
-		}
-		var inHand = this.getCardsInHand(card);
-		if(card.type == "cat"){
-			return inHand.length > 1;
-		}else{
-			return inHand.length > 0;
-		}
-	},
-
-	onEKDefuse: function(options){
-		this.isExploding = false;
-
-		//The kitten was placed back into deck
-		this.get("gameState").deckCount++;
-	},
-
-	onPlayerExploded: function(message){
-		if(this.isMe(message.player)){
-			this.get("gameState").hand = [];
-		}
-		this.getPlayerById(message.player).handSize = 0;
-		if(!this.get("gameState").exploded){
-			this.get("gameState").exploded = [];
-		}
-		this.get("gameState").exploded.push(message.player);
-	},
-
-	isExploded: function(player){
-		return this.get("gameState").exploded && this.get("gameState").exploded.indexOf(player) > -1;
-	},
-
-	onCardPlayed: function(options){
-		if(this.isMe(options.from)){
-			this.removeCardFromHand(options.card);
-			if(options.card.type == "cat"){
-				this.removeCardFromHand(options.card);
-			}
-		}else{
-			var p = this.getPlayerById(options.from);
-			p.handSize--;
-			if(options.card.type == "cat"){
-				p.handSize--;
-			}
-		}
-		this.get("gameState").pile.push(options.card);
-		if(options.card.type == "cat"){
-			this.get("gameState").pile.push(options.card);
-		}
-	},
-
-	onEKDrawn: function(message){
-		this.get("gameState").deckCount--;
-		if(this.isMe(message.player)){
-			this.isExploding = true;
-		}
-	},
-
-	onPlayerWin: function(message){
-		this.isExploding = false;
-		this.set("status", 3);
-
-	},
-
-	removeCardFromHand: function(card){
-		var hand = this.get("gameState").hand;
-		for(var i=0; i<hand.length; i++){
-			if(hand[i].id == card.id && hand[i].image == card.image){
-				hand = hand.splice(i, 1);
-				this.getPlayerById(this.socket.id).handSize--;
-				return;
-			}
-		}
-		console.error("Tried to remove a card that doesn't exist");
-	},
-
-	getPlayerById: function(id){
-		return this.get("players").filter(function(player){
-			return player.id == id;
-		})[0];
-	},
-
-	getCardsInHand: function(card){
-		var gameState = this.get("gameState");
-		if(gameState && gameState.hand){
-			return gameState.hand.filter(function(handCard){
-				return handCard.id == card.id && handCard.image == card.image;
-			});
-		}
-		return [];
-	},
-
-	addCardToHand: function(card){
-		this.get("gameState").hand.push(card);
-		this.getPlayerById(this.socket.id).handSize++;
-	},
-
-	onMoveCard: function(options){
-		if(this.isMe(options.to)){
-			this.addCardToHand(options.card);
-			this.getPlayerById(options.from).handSize--;
-		}else if(this.isMe(options.from)){
-			this.removeCardFromHand(options.card);
-			this.getPlayerById(options.to).handSize++;
-		}else{
-			this.getPlayerById(options.from).handSize--;
-			this.getPlayerById(options.to).handSize++;
-		}
-	},
-
-	drawCard: function(){
-		if(this.inProgress() && this.isMyTurn() && !this.isExploding){
-			this.socket.emit("gameMessage", {
-				command: "drawCard",
-				roomId: this.get("id")
-			});
-		}
-	},
-
-	rotatePlayers: function(){
-		var players = this.get("players");
-		if(players && players.length){
-			while(!this.isMe(players[0].id)){
-				players.unshift(players.pop());
-			}
-		}
-	},
-
-	startRoom: function(){
-		this.socket.emit("gameMessage", {
-			command: "startGame",
-			roomId: this.get("id")
-		});
-	},
-
-	inProgress: function(){
-		return this.get("status") == 2;
-	},
-
-	isMyTurn: function(){
-		return this.get("gameState") && this.isMe(this.get("gameState").turnPlayer);
-	},
-
-	isMe: function(id){
-		return this.socket.id == id;
-	}
-
+  onPlayerWin: function(message){
+    this.isExploding = false;
+    this.set("status", 3);
+  },
 });
 
 module.exports = ExplodingKitten;
